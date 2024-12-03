da8f9db066792f1a1921f8b7dc1e587883de1acc
GauravVKulkarni <gauravvilaskulkarni9211@gmail.com>
2024-12-03 20:44:15 +0530

    .

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,8 @@
 // components/Banner.jsx
-const Banner = () => {
+const Banner = ({text}) => {
     return (
       <div className="bg-gradient-to-r from-blue-900 to-blue-600 text-white text-left pt-40 pb-20">
-        <h1 className="text-2xl font-semibold px-16 py20">Careers</h1>
+        <h1 className="text-2xl font-semibold px-16 py20">{text}</h1>
       </div>
     );
   };
